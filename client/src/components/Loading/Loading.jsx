74bc57348e8de43926f24e6d1d982d7fc5dfa74b
Silvia9680 <silviacarolina9680@gmail.com>
2022-11-29 20:51:07 -0300

    Create Loading.jsx

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Load from '../media/Loading.gif';
+
+
+function Loading(){
+    
+    return(
+        <div>
+            <img src = {Load} alt= "Loading" width= '500px' height='500px'/>
+        </div>
+    )
+
+}
